7e75ea8ed22033735b8391e0397e9e4c300334dc
juliadyrdal <juliadyrdal@protonmail.com>
2023-09-20 20:49:34 +0200

    uninstall nuxt-swiper for testing

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,7 +8,6 @@ export default defineNuxtConfig({
     "@nuxt/image",
     "@nuxtjs/google-fonts",
     "@nuxtjs/i18n",
-    "nuxt-swiper",
   ],
   i18n: {
     vueI18n: "./i18n.config.ts",
@@ -26,13 +25,6 @@ export default defineNuxtConfig({
     detectBrowserLanguage: {
       useCookie: false,
     },
-  },
-  swiper: {
-    // Swiper options
-    //----------------------
-    prefix: "Swiper",
-    // styleLang: 'css',
-    modules: ["navigation", "pagination", "a11y", "virtual", "thumbs", "autoplay"], // all modules are imported by default
   },
   googleFonts: {
     families: {
