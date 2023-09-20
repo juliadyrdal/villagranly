fd340009b7f99cdaa42c2804f64b581cc8dd676a
juliadyrdal <juliadyrdal@protonmail.com>
2023-09-20 20:35:36 +0200

    remove nuxt-swiper for testing

--- components/PhotoSlide.vue
+++ components/PhotoSlide.vue
@@ -3,7 +3,7 @@
         aspect-[3/2] w-full bg-gray-50 object-cover lg:absolute lg:inset-0 lg:aspect-auto lg:h-full
           </vue-picture-swipe> -->
   
-      <Swiper
+      <!--<Swiper
         :modules="[SwiperNavigation, SwiperPagination, SwiperA11y, SwiperVirtual, SwiperAutoplay]"
         :slides-per-view="1"
         :space-between="50"
@@ -29,6 +29,7 @@
           <ButtonNext /> 
         </div>
       </Swiper>
+      -->
   </template>
   
   <script setup>
@@ -36,7 +37,7 @@
   import imageTwo from "@/assets/images/hero-gallery/granly102.jpg";
   import imageThree from "@/assets/images/hero-gallery/granly113.jpg";
 
-  const swiper = useSwiper()
+  //const swiper = useSwiper()
   
   //     {src: 'images/gallery/_DSC1407.jpg',thumbnail: 'images/gallery/_DSC1407.jpg',w: 1200, h: 900, title: 'Stue'},
   </script>
