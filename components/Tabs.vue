edab81406c50bf6f0a21c1cb23f1ab885336b8c9
juliadyrdal <juliadyrdal@protonmail.com>
2023-11-28 08:17:52 +0100

    fix tabSwitcher bug on photogallery view

--- components/Tabs.vue
+++ components/Tabs.vue
@@ -1,77 +1,40 @@
-<!--
-  This example requires some changes to your config:
-  
-  ```
-  // tailwind.config.js
-  module.exports = {
-    // ...
-    plugins: [
-      // ...
-      require('@tailwindcss/forms'),
-    ],
-  }
-  ```
-  {{ tabLocaleNo ? galleryTabs.nameNo : galleryTabs.nameEn }}
--->
 <template>
-    <div class="pb-4">
-      <div class="sm:hidden">
-        <label for="galleryTabs" class="sr-only">Select a tab</label>
-        <!-- Use an "onChange" listener to redirect the user to the selected tab URL. -->
-        <select id="galleryTabs" name="galleryTabs" class="block w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500">
-          <option v-for="tab in galleryTabs" :key="tab.name" :selected="tab.current">{{ tab.name }}</option>
-        </select>
-      </div>
-      <div class="hidden sm:block">
-        <nav class="flex space-x-4" aria-label="galleryTabs">
-          <a @click="tabClick(tab)" v-for="tab in galleryTabs" :key="tab.name" :class="[tab.current ? 'bg-white px-5 py-2 text-sm font-sans font-medium text-theme-gray-dark border-gray-600 border-2 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600' : 'text-gray-500 hover:text-gray-700', 'px-3 py-2 text-sm font-medium cursor-pointer']" :aria-current="tab.current ? 'page' : undefined">{{ locale === 'no' ? tab.nameNo : tab.nameEn }}</a>
-        </nav>
-      </div>
+  <div class="pb-4">
+    <div class="sm:hidden">
+      <label for="galleryTabs" class="sr-only">Select a tab</label>
+      <select id="galleryTabs" name="galleryTabs" class="block w-full rounded-md border-gray-300 focus:border-indigo-500 focus:ring-indigo-500">
+        <option v-for="tab in galleryTabs" :key="tab.name" :selected="tab.current">{{ tab.name }}</option>
+      </select>
     </div>
-  </template>
-  
-  <script setup>
-import { TabList } from '@headlessui/vue';
+    <div class="hidden sm:block">
+      <nav class="flex space-x-4" aria-label="galleryTabs">
+        <a 
+          v-for="tab in galleryTabs" 
+          :key="tab.name" 
+          :class="[tab.current ? 'bg-white px-5 py-2 text-sm font-sans font-medium text-theme-gray-dark border-gray-600 border-2 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600' : 'text-gray-500 hover:text-gray-700', 'px-3 py-2 text-sm font-medium cursor-pointer']" 
+          @click="emitTabEvent(tab.href)"
+          :aria-current="tab.current ? 'page' : undefined"
+        >
+          {{ locale === 'no' ? tab.nameNo : tab.nameEn }}
+        </a>
+      </nav>
+    </div>
+  </div>
+</template>
 
-const { locale, locales, messages } = useI18n();
+<script setup>
+import { defineProps, defineEmits } from 'vue';
+import { useI18n } from 'vue-i18n';
 
-const props = defineProps(['galleryTabs'])
+const { locale } = useI18n();
+const props = defineProps(['galleryTabs']);
+const emit = defineEmits(['tab-event']);
 
-const emit = defineEmits([]);
+const emitTabEvent = (href) => {
+  emit('tab-event', href);
+};
+</script>
 
-const tabsNo = [
-  { name: 'Hovedhuset interiør', href: 'main', current: true },
-  { name: 'Gjesteboligen', href: 'guest', current: false },
-  { name: 'Annekset', href: 'annex', current: false },
-  { name: 'Garasjen', href: 'garage', current: false },
-  { name: 'Hovedhuset og parken', href: 'park', current: false },
-  { name: 'Sjøhuset', href: 'sea', current: false },
-  { name: 'Nøstet', href: 'boat', current: false },
-]
-
-const tabsEn = [
-  { name: 'Main house interior', href: 'main', current: true },
-  { name: 'Guest house', href: 'guest', current: false },
-  { name: 'Annex', href: 'annex', current: false },
-  { name: 'Garage', href: 'garage', current: false },
-  { name: 'Main house and park', href: 'park', current: false },
-  { name: 'Sea house', href: 'sea', current: false },
-  { name: 'Boat house', href: 'boat', current: false },
-]
- 
-
-function testTab(n) {
-  console.log(n.href)
-  console.log(tabs.value[2].href)
- for (let i = 0; i < tabs.value.length; i++) {
-  
-  if (n.href === tabs.value[i].href) {
-    console.log('YES')
-  } else {
-    console.log('no')
-  }
- }
-}
-
-
-  </script>
+<style scoped>
+  /* Your CSS styles here */
+</style>
--- pages/photogallery.vue
+++ pages/photogallery.vue
@@ -1,4 +1,5 @@
 <script setup>
+// Setting up the page title and SEO metadata for the component
 useHead({
   title: 'Bilder',
 })
@@ -10,8 +11,10 @@ useSeoMeta({
   ogDescription: 'Unik og særpreget villa som gir assosiasjoner til vikingetidens høvdinggårder og tradisjonell stølsarkitektur. Villa Granly er omkranset av en vakker park med strandlinje til Kviturspollen.',
   ogImage: 'https://villa-granly.no/_nuxt/_DSC1694.2ee02c40.jpg',
 })
-  
-  const galleryTabs = [
+
+// Reactive state for managing tabs and their active status
+const state = reactive({
+  galleryTabs: [
   { nameEn: 'Main house interior', nameNo: 'Hovedhuset interiør', href: 'main', current: true },
   { nameEn: 'Guest house', nameNo: 'Gjesteboligen', href: 'guest', current: false },
   { nameEn: 'Annex', nameNo: 'Annekset', href: 'annex', current: false },
@@ -19,71 +22,47 @@ useSeoMeta({
   { nameEn: 'Main house and park', nameNo: 'Hovedhuset og parken', href: 'park', current: false },
   { nameEn: 'Sea house', nameNo: 'Sjøhuset', href: 'sea', current: false },
   { nameEn: 'Boat house', nameNo: 'Nøstet', href: 'boat', current: false },
-]
+  ],
+})
 
-  const mainTab = ref(true);
-  const guestTab = ref(false);
-  const annexTab = ref(false);
-  const garageTab = ref(false);
-  const parkTab = ref(false);
-  const seaTab = ref(false);
-  const boatTab = ref(false);
 
-  function tabSwitch(n) {
-    for (let i = 0; i < galleryTabs.length; i++) {
-   console.log('working')
-    galleryTabs[i].current = !galleryTabs.current;
-  }
- 
 
-    console.log("This is n" + n)
-    mainTab.value = false;
-    guestTab.value = false;
-    annexTab.value = false;
-    garageTab.value = false;
-    parkTab.value = false;
-    seaTab.value = false;
-    boatTab.value = false;
-
-    if (n === 'main') {
-      return mainTab.value = true;
-    } else if (n === 'guest') {
-      return guestTab.value = true;
-    } else if (n === 'annex') {
-      return annexTab.value = true;
-    } else if (n === 'garage') {
-      return garageTab.value = true;
-    } else if (n === 'park') {
-      return parkTab.value = true;
-    } else if (n === 'sea') {
-      return seaTab.value = true;
-    } else if (n === 'boat') {
-      return boatTab.value = true;
+// Function to handle the switching of tabs
+// This function updates the 'current' property of each tab based on the selected tab's href
+function tabSwitch(selectedTabHref) {
+  console.log('test tabSwitcher code')
+  console.log(selectedTabHref)
+  state.galleryTabs.forEach(tab => {
+    if (tab.href === selectedTabHref) {
+      tab.current = true;
+    } else {
+      tab.current = false;
     }
-  }
+  });
+}
+ 
 
 </script>
 
 <template>
     <div>
-      <!--<TheHeader @scroll-to="goTo()" />-->
-      <PageHeader
-        title="pages.photoGallery.title"
-        body=""
-      />
-      <PageSection>
-        <Tabs @tab-event="tabSwitch" :galleryTabs="galleryTabs" />
-      </PageSection>
-      <PageSection>
-        <PhotoGalleryMain v-if="mainTab===true" />
-        <PhotoGalleryGuest v-if="guestTab===true" />
-        <PhotoGalleryAnnex v-if="annexTab===true" />
-        <PhotoGalleryGarage v-if="garageTab===true" />
-        <PhotoGalleryPark v-if="parkTab===true" />
-        <PhotoGallerySea v-if="seaTab===true" />
-        <PhotoGalleryBoatHouse v-if="boatTab===true" />
-      </PageSection>
-    </div>
+    <PageHeader
+      title="pages.photoGallery.title"
+      body=""
+    />
+    <PageSection>
+      <Tabs @tab-event="tabSwitch" :galleryTabs="state.galleryTabs" />
+    </PageSection>
+    <PageSection>
+      <PhotoGalleryMain v-if="state.galleryTabs[0].current" />
+      <PhotoGalleryGuest v-if="state.galleryTabs[1].current" />
+      <PhotoGalleryAnnex v-if="state.galleryTabs[2].current" />
+      <PhotoGalleryGarage v-if="state.galleryTabs[3].current" />
+      <PhotoGalleryPark v-if="state.galleryTabs[4].current" />
+      <PhotoGallerySea v-if="state.galleryTabs[5].current" />
+      <PhotoGalleryBoatHouse v-if="state.galleryTabs[6].current" />
+    </PageSection>
+  </div>
   </template>
   
   <style lang="scss" scoped></style>
